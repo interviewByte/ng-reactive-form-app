b4964a5570ac2f73f3c2bda4d90f9fbb5485bc68
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-12-13 23:46:07 +0530

    Integrated nested from group and integrated how we can update form group value in html view template

--- src/app/components/grouping-form/grouping-form.component.ts
+++ src/app/components/grouping-form/grouping-form.component.ts
@@ -10,9 +10,23 @@ export class GroupingFormComponent {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
   });
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
+  }
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
   }
 }
